7af7660cc4903f11835b1a28d07a49b65cfa9b83
pbkakiuchi <kakiuchi@primebrains.co.jp>
2021-10-12 17:32:37 +0900

    fix2

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 // webAssembly file nameをhashにすると、buildこける。
 module.exports = {
   disDir: '.next',
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-    };
-    // hash 使えない。。。(´･ω･`)
-    config.output.webassemblyModuleFilename =
-      (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-    return config;
-  },
 };
-
-// /**
-//  * @type { import("next").NextConfig}
-//  */
-// const config = {
-//   webpack: (config) => ({
-//     ...config,
-//     experiments: {
-//       asyncWebAssembly: true,
-//     },
-//   }),
-// };
-// module.exports = config;
